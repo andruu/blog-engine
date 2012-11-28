69508eb69e9e88e0358b3e6877697f0fb94ebcb4
andruu <andru.weir@gmail.com>
2012-11-27 21:32:31 -0500

    Trying to fix smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,9 @@ Blog::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'], 
     :address        => ENV['MAILGUN_SMTP_SERVER'],
