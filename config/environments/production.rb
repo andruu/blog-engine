e7d4af97da8ff5b111feb77b6cd386cfc7ad8a85
andruu <andru.weir@gmail.com>
2012-11-27 21:50:44 -0500

    Temporarily removing email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,17 +51,6 @@ Blog::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'], 
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'andru.co',
-    :authentication => :plain,
-  }
-  config.action_mailer.default_url_options = { host: "andru.co" }
-
   # Enable threaded mode
   # config.threadsafe!
 
