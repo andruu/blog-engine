ae29f5086a001b64bd2f0cd9ec3712ac1ac93425
andruu <andru.weir@gmail.com>
2012-11-30 11:25:11 -0500

    Changed blurb

--- config/initializers/blog.rb
+++ config/initializers/blog.rb
@@ -28,7 +28,7 @@ module Blog
 
   # Sidebar
   NAME         = 'Andrew Weir'
-  BLURB        = 'Web developer specializing in Ruby, Node.js and PHP.'
+  BLURB        = 'Entrepreneur & web developer specializing in Ruby, Node.js and PHP.'
   GITHUB       = 'andruu'
   TWITTER      = 'andruu'
   LINKEDIN     = 'andruu'
